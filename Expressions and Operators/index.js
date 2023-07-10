15ec6b11329b86385b5c28420d1951b7ac1fe730
Melihomac <melihomac@hotmail.com>
2023-07-10 13:23:22 +0300

    Expressions and Operators

--- Expressions and Operators/index.js
+++ Expressions and Operators/index.js
@@ -7,3 +7,13 @@ const key = "y";
 obj[key] = 5;
 console.log(obj[key]); // Prints 5.
 console.log(obj); // Prints { x: 3, y: 5 }.
+
+let username = "" // eğer içeriye "Melih" yazarsam hello Melih diyecekti
+greeting = "hello " + (username ? username : "there");
+console.log(greeting) // hello there
+
+let maxWidth = 1000;
+// If maxWidth is defined, use that. Otherwise, look for a value in
+// the preferences object. If that is not defined, use a hardcoded constant.
+let max = maxWidth ?? preferences.maxWidth ?? 500;
+console.log(max)
